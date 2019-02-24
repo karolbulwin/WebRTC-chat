c37ff5b2d36f01f6805fc6c8f29691d28c72f652
Karol Bulwin <karolbulwin@gmail.com>
2019-02-24 13:34:01 +0100

    sw

---
+++ sw.js
@@ -0,0 +1,72 @@
+const CACHE_NAME = 'chat-cache-v1';
+const urlsToCache = [
+  '/',
+  'chat.html',
+  'favicon.ico',
+  'public/stylesheets/main.css',
+  'public/javascripts/main.js',
+  'public/javascripts/serwice-worker.js',
+  'https://fonts.googleapis.com/css?family=Open+Sans',
+  'https://cdn.jsdelivr.net/npm/peerjs@0.3.20/dist/peer.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['chat-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          (res) => {
+            // Check if we received a valid response
+            if (!res || res.status !== 200 || res.type !== 'basic') {
+              return res;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            const responseToCache = res.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
