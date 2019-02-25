0fd3b4db110c87dd5973d7c72400e1e7d576436d
Karol Bulwin <karolbulwin@gmail.com>
2019-02-25 22:58:06 +0100

    better but  still on second click...

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -136,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#data-to-send').addEventListener('click', () => {
     setTimeout(() => {
       showLastMessageOnTheBottom();
-    }, 150);
+    }, 250);
   });
 
   function hideInfo() {
