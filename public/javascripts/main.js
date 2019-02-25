878908ec05a4bc5378293bc8250b31c112bb91ed
Karol Bulwin <karolbulwin@gmail.com>
2019-02-25 23:00:58 +0100

    still on second click

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -136,7 +136,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#data-to-send').addEventListener('click', () => {
     setTimeout(() => {
       showLastMessageOnTheBottom();
-    }, 250);
+    }, 1250);
   });
 
   function hideInfo() {
